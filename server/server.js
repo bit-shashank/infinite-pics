dd2a9a188bc0a53ff5dbd8fe51c58a4d493aa3fb
bit-shashank <shashankcs083@gmail.com>
2021-07-01 12:43:43 +0530

    Updated Frontend

--- server/server.js
+++ server/server.js
@@ -4,19 +4,30 @@ const app = express();
 const port = process.env.PORT || 3000;
 const axios = require("axios");
 var cheerio = require("cheerio");
+var cors = require('cors')
 
+app.use(cors())
 app.set("port", port);
 
 app.listen(port, () => console.log(`App started on port ${port}.`));
 
 app.get("/images/:query", async (req, res) => {
 	word = await getNextWord(req.params.query);
-	let images = await getImages(word);
-	res.json(images);
+
+	let images = await getImages(req.params.query);
+    if(images)
+	    res.json({ nextWord: word, images: images });
+    else{
+        images=[]
+        res.json({ nextWord: word, images: images });
+    }
 });
 
 async function getImages(query) {
-	let url ="https://www.google.com/search?q="+query+"&sxsrf=ALeKk01qKl915_4HGWDiNBbVJ8j-6oqygA:1625119286346&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiPt56OmcHxAhWIc30KHQE8BjAQ_AUoAXoECAEQAw&biw=1366&bih=663";
+	let url =
+		"https://www.google.com/search?q=" +
+		query +
+		"&sxsrf=ALeKk01qKl915_4HGWDiNBbVJ8j-6oqygA:1625119286346&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiPt56OmcHxAhWIc30KHQE8BjAQ_AUoAXoECAEQAw&biw=1366&bih=663";
 	try {
 		const { data } = await axios.get(url);
 		let $ = cheerio.load(data);
@@ -24,8 +35,7 @@ async function getImages(query) {
 		let sources = [];
 		$(images).each(function (i, img) {
 			let src = $(img).attr("src");
-            if (src.startsWith("https"))
-			    sources.push(src);
+			if (src.startsWith("https")) sources.push(src);
 		});
 		return sources;
 	} catch (err) {
@@ -49,14 +59,17 @@ async function getNextWord(currWord) {
 				url != undefined &&
 				url.startsWith("/wiki/") &&
 				text.indexOf(" ") == -1 &&
-				text.length >= 4
+				text.length >= 2
 			)
-				nextWords.push(text);
+			nextWords.push(text);
 		});
+        if (nextWords.length==0)
+            return "death"
+
 		let word = nextWords[Math.floor(Math.random() * nextWords.length)];
 		return word;
 	} catch (err) {
 		console.log(err);
-		return "error";
+		return "death";
 	}
 }
