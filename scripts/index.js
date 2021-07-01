237b67c69ddb039b08de8c7340de3acf41e7e730
bit-shashank <shashankcs083@gmail.com>
2021-07-01 12:43:30 +0530

    Updated Frontend

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,36 @@
 let formContainer = document.getElementById("formCon");
 let imgContainer = document.getElementById("imgCon");
-
 let imgCols = document.getElementsByClassName("column");
+let keyword;
 function explore() {
 	formContainer.classList.add("hidden");
 	imgContainer.classList.remove("hidden");
+	keyword=document.getElementById("wordInp").value;
+	if (keyword==undefined)
+		keyword="Love"
 	showImages();
 }
 
-function scrapImages(keyword) {
-	
+async function scrapImages() {
+	let url="http://localhost:3000/images/"+keyword;
+	let res=await fetch(encodeURI(url),{
+		method: 'GET',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+	})
+
+	data= await res.json();
+	keyword=data.nextWord;
+	return data.images;
 }
 
-function showImages() {
-	scrapImages("banana");
-	for (let j = 0; j <= 10; j++) {
-		for (var i = 0; i < 4; i++) {
+async function showImages() {
+	images=await scrapImages(keyword);
+	for (let i = 0; i < images.length; i++) {
 			let img = document.createElement("img");
-			let width = Math.floor(Math.random() * 100 + 100);
-			let height = Math.floor(Math.random() * 100 + 100);
-			img.src = "https://picsum.photos/" + width + "/" + height;
+			img.src = images[i];
 			imgCols[i % 4].appendChild(img);
-		}
 	}
 }
 
